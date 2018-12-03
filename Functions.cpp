c289c52fd7d86dfd5a920c2073af8815cf149c5c
hadsan <hadsan7@gmail.com>
2018-12-03 01:49:02 +0100

    Add files via upload

---
+++ Functions.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "Point.h"
+#include "Line.h"
+#include "Triangle.h"
+#include "Polygon.h"
+#include "Shape.h"
+
+
+using namespace std;
+
+int getPoints(std::string,double*&);
+int getPoints(string filename,double *&points)
+{
+    double num;
+    int SIZE = 0;
+    ifstream input;
+    input.open(filename);
+    
+    if(input.is_open())
+    { 
+        
+        while (input >> num)
+        {
+            SIZE++;
+        } 
+        input.close();
+        
+        points = new double[SIZE];
+        input.open(filename);
+        if(input.is_open())
+        {
+            int i = 0;
+            while (input >> points[i])
+            {
+                i++;
+            } 
+            input.close();
+        
+        }
+    }
+    else
+    {
+        std::cout << "ERROR" << std::endl;
+    }
+    return SIZE;
+}
+
+void getShape(Shape*);
+void getShape(Shape *pointer)
+{
+    
+    Shape *pointer1;
+    double* testpoint = new double[2];
+    testpoint[0] =0;
+    testpoint[1] = 0;
+    pointer1 = new Point(testpoint);
+    
+    cout<<" -Shape type: "<< pointer->getType()<<endl;
+    cout<<" -Convex: "<<pointer->isConvex()<<endl;
+    cout<<" -Circumference: "<<pointer->circumference()<<endl;
+    cout<<" -Area: "<<pointer->area()<<endl;
+    cout<<" -Center point:  ("<<pointer->position()[0]<<","<<pointer->position()[1]<<")"<<endl;
+    cout<<" -Distance from test point ("<<testpoint[0]<<","<<testpoint[1]<<"):  is "<<pointer->distance(pointer1)<<endl;
+    
+    
+}
